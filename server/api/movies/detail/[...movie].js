47d7d01a5e90711f637223cb5f2068b9445b41be
adityayfn <adhitya.firstian@gmail.com>
2023-10-23 11:46:59 +0700

    .

--- server/api/movies/detail/[...movie].js
+++ server/api/movies/detail/[...movie].js
@@ -122,7 +122,7 @@ export async function getStreamingLinks(streamLinks) {
 
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: "/usr/bin/google-chrome",
+    executablePath: "/usr/bin/chromium-browser",
     cacheDirectory: "~/.cache/puppeteer",
   })
 
